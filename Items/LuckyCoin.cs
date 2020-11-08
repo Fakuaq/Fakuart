6930a3740bd36640697ecd98950231a0e063bfeb
Fakuaq <54741191+Fakuaq@users.noreply.github.com>
2020-11-08 18:28:54 +0200

    added LuckyCoin item

---
+++ Items/LuckyCoin.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fakuart.Items
+{
+    public class LuckyCoin : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+           DisplayName.SetDefault("Lucky coin");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.rare = ItemRarityID.Yellow;
+            item.value = Item.sellPrice(gold: 10);
+            item.holdStyle = 1;
+            //item.useAnimation = 10;
+        }
+        public override void HoldItem(Player player)
+        {
+            player.noFallDmg = true;
+        }
+
+    }
+}
