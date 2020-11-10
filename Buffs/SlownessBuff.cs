c69a00dd5070a286b64ddd13f0ab0c38a1a73f43
Fakuaq <54741191+Fakuaq@users.noreply.github.com>
2020-11-10 15:18:06 +0200

    Added icy staff and slow debugg

---
+++ Buffs/SlownessBuff.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Fakuart.Buffs
+{
+    public class SlownessBuff : ModBuff
+    {
+        bool slow = false;
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Slowness");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+            
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (!slow) 
+            {
+                npc.velocity.X *= 0.75f;
+                slow = true;
+            }
+            else
+            {
+                slow = false;
+            }
+        }
+    }
+}
---
+++ Items/IcyStaff.cs
@@ -0,0 +1,58 @@
+﻿using IL.Terraria.UI;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
+using Fakuart.Projectiles;
+
+namespace Fakuart.Items
+{
+    public class IcyStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Icy staff");
+            Item.staff[item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.SapphireStaff);
+            item.damage = 14;
+            item.magic = true;
+            item.mana = 12;
+            item.width = 16;
+            item.height = 16;
+            item.useTime = 25;  
+            item.useAnimation = 25;
+            item.rare = ItemRarityID.Green;
+            item.value = Item.sellPrice(gold: 2);
+            item.noMelee = true;
+            item.knockBack = 1;
+            item.shoot = ProjectileType<IceProjectile>();
+            item.shootSpeed = 12f;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.autoReuse = false;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 speed = new Vector2(speedX, speedY);
+            Vector2 positionOffset = new Vector2(Main.rand.Next(-125, 125), Main.rand.Next(-75, 0));
+            Vector2 newPosition = Vector2.Add(position, positionOffset);
+            Projectile.NewProjectile(newPosition, speed, type, item.damage, knockBack, player.whoAmI);
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.LeadBar, 10);
+            recipe.AddIngredient(ItemID.Sapphire, 5);
+            recipe.AddIngredient(ItemID.IceBlock, 30);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
